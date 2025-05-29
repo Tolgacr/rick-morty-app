bf5ed02b851767d0ef2eec11b478005679f3b42b
Tolga Gacar <tolgagacar35@gmail.com>
2025-05-29 22:06:55 +0300

    Initial commit by Tolga

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-blue-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        <span role="img" aria-label="smiling face">😊</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tailwind'in css'i nerelerde arayacağını belirtir
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
